2ad64829fe358ef79fde137bec900c092f726528
Nguyengod <106512091+Nguyengod@users.noreply.github.com>
2022-05-30 15:46:42 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     titleWeb: "gửi cho baby",
-    introTitle: 'bé tiên❤',
+    introTitle: 'công chúa tiên <3',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -12,7 +12,7 @@ const CONFIG = {
     btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'tại vì mình cũng yêu bạn <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
     messDesc: 'hôm nào rảnh, mình qua đón bạn đi chơi nha.',
     btnAccept: 'Okiiiii lun <3',
